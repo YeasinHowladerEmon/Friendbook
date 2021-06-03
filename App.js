2e4b02cd3121abf8231995d79f1370e48b7e7eea
emonibnsalim <emonibnsali@gmail.com>
2021-06-03 19:58:28 +0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>hello I am here!</Text>
     </View>
   );
 }
@@ -14,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
 });
